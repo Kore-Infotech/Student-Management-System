1750385e0801faa8be705103e0cc449baf44783e
archanashetty <archana_shetty@yahoo.co.in>
2015-05-25 10:53:04 +0800

    UserRole changes

--- src/main/java/com/koreinfotech/sms/service/UserServiceImpl.java
+++ src/main/java/com/koreinfotech/sms/service/UserServiceImpl.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,6 +17,7 @@ import org.springframework.stereotype.Service;
 
 import com.koreinfotech.sms.dao.UserDAO;
 import com.koreinfotech.sms.domain.User;
+import com.koreinfotech.sms.domain.UserRole;
 
 @Service("userService")
 public class UserServiceImpl implements UserDetailsService,UserService {
@@ -51,23 +51,29 @@ public class UserServiceImpl implements UserDetailsService,UserService {
 	 */
 	private org.springframework.security.core.userdetails.User buildUserForAuthentication(com.koreinfotech.sms.domain.User user){
 		
-		return new org.springframework.security.core.userdetails.User(user.getUserName(),user.getPassword(),user.isEnabled(),true,true,true,buildUserAuthority());
+		return new org.springframework.security.core.userdetails.User(user.getUserName(),user.getPassword(),user.isEnabled(),true,true,true,buildUserAuthority(user.getUserRoles()));
 		
 	}
 	/**
-	 * UPDATE THIS LATER TO GET ROLES FROM THE DATABASE
+	 * Build user's authorities
 	 * @return
 	 */
-	private List<GrantedAuthority> buildUserAuthority() {
+	private List<GrantedAuthority> buildUserAuthority(List<UserRole> userRoles) {
 		
 		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
- 
+		for(UserRole userRole : userRoles) {
+			logger.info("User Role :" + userRole.getRole());
+			authorities.add(new SimpleGrantedAuthority(userRole.getRole()));
+		}
 		return authorities;
 	}
 	
 	@Transactional
 	public void addUser(User user) {
+		
+		//Adding role "ROLE_USER" by default for all users 
+		
+		user.addUserRole(new UserRole("ROLE_USER"));
 		userDAO.addUser(user);
 	}
 
